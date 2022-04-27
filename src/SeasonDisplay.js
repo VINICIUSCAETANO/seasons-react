11ef5386a4ad243546089119a680742f1df03924
Vinicius Caetano <viniciusbeschcaetano@gmail.com>
2022-04-27 14:12:35 -0300

    Adiciona funcão para retornar estação pela localização

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+};
+
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const outText = season === 'winter' ? 'Chilly' : 'hot'
+
+    return <div>
+        <h1>
+            {outText}
+        </h1>
+    </div>
+};
+
+export default SeasonDisplay;
