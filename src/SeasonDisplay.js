12035a32fceec6a36c81546cb6d2c8ac0e698af5
Vinicius Caetano <viniciusbeschcaetano@gmail.com>
2022-04-27 14:26:08 -0300

    Icones aparecendo

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -12,11 +12,12 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
     const outText = season === 'winter' ? 'Chilly' : 'hot'
+    const icon = 'season' === 'winter' ? 'snowflake' : 'sun';
 
     return <div>
-        <h1>
-            {outText}
-        </h1>
+        <i className={`${icon} icon`} />
+        <h1>{outText}</h1>
+        <i className={`${icon} icon`} />
     </div>
 };
 
